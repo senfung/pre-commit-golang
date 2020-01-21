fef109862aa69d9aba7ac5bddb19adcab1c95e15
Felix Hui <huisenfung@gmail.com>
2020-01-21 17:10:43 +0800

    Update run-go-lint.sh

--- run-go-lint.sh
+++ run-go-lint.sh
@@ -4,12 +4,15 @@ PASS=true
 for FILE in "$@"
 do
     output=$(golint "$FILE" 2>&1 )
+    echo $FILE
     if ! [ -z "$output" ]
     then
         PASS=false
 	echo "$output"
     fi
 done
+
+echo $PASS
 
 if [ "$PASS" = "false" ]; then
     exit 1
